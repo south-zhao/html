41f3b9b1911f848ef06c751d7f27cfe75f2e6892
south-zhao <2252585312@qq.com>
2022-09-05 21:47:39 +0800

    js

--- js/1.js
+++ js/1.js
@@ -1,7 +1,14 @@
-var a = "你好呀";
-b = 35;
-console.log(a);
-console.log(b);
-c = { name: "jajis", age: 18 };
-console.log(c.name);
-console.log(c['age'])
+let a1 = "你好呀"
+// b = 35
+// console.log(a)
+// console.log(b)
+// c = { name: "jajis", age: 18 }
+// console.log(c.name)
+// console.log(c['age'])
+
+let a = 1
+let b = '1'
+console.log(a+b)
+console.log(a-b)
+console.log(`我叫${a1}`)
+
