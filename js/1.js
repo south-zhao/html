578c67499af1ebd4786f2c586a8a879d492c1d60
south-zhao <2252585312@qq.com>
2022-09-08 10:03:13 +0800

    九九乘法表

--- js/1.js
+++ js/1.js
@@ -1,13 +1,22 @@
-let a1 = "你好呀"
+// let a1 = "你好呀"
 // b = 35
 // console.log(a)
 // console.log(b)
 // c = { name: "jajis", age: 18 }
 // console.log(c.name)
 // console.log(c['age'])
-let a = 1
-let b = '1'
-console.log(a+b)
-console.log(a-b)
-console.log(`我叫${a1}`)
+// let a = 1
+// let b = '1'
+// console.log(a+b)
+// console.log(a-b)
+// console.log(`我叫${a1}`)
 
+// let i = 10
+// console.log(i++)
+// console.log(++i)
+
+for(let i=1;i<10;i++){
+    for(let j=1;j<=i;j++){
+        console.log(j+"*"+i+"="+j*i)
+    }
+}
