aeec92642434883e75c4d4f88aaf41ccf8614a92
south-zhao <2252585312@qq.com>
2022-10-17 16:55:40 +0800

    html

--- js/1.js
+++ js/1.js
@@ -15,8 +15,8 @@
 // console.log(i++)
 // console.log(++i)
 
-for(let i=1;i<10;i++){
-    for(let j=1;j<=i;j++){
-        console.log(j+"*"+i+"="+j*i)
+for (let i = 1; i < 10; i++) {
+    for (let j = 1; j <= i; j++) {
+        console.log(j + "*" + i + "=" + j * i)
     }
 }
